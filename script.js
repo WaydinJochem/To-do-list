56a33aeabbe64215faa3b12021aaf6164f43c618
WaydinJochem <jochem7405@gmail.com>
2023-06-03 10:04:16 +0200

    Figuring it out

--- script.js
+++ script.js
@@ -2,8 +2,20 @@
 let array1 = [];
 let btnAdd = document.getElementById("Add");
 
+
+
 btnAdd.addEventListener("click", () => {
-    let input = document.getElementById("input").value;
+    let input = {list: document.getElementById("input").value};
     array1.push(input);
-    document.getElementById("display").innerHTML = array1;
+
+    let listItems = document.querySelector(".lists");
+    listItems.innerHTML = "";
+    array1.forEach((data) =>{
+        listItems.innerHTML += 
+        `<div class="list-item">
+            <input type="checkbox">
+            <h3>${data.list}</h3>
+            <button id='x'>X</button>
+        </div>`;
+    });
 });
