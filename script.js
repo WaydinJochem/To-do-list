d64476026479b5aaa52e1a7a364d68d3e1045e5c
WaydinJochem <jochem7405@gmail.com>
2023-06-03 14:21:17 +0200

    Struggling again

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 
 let array1 = [];
 let btnAdd = document.getElementById("Add");
+let btnSort = document.getElementById("Sort");
+querySelector
 
-
-
+// list items and add the to array
 btnAdd.addEventListener("click", () => {
-    let input = {list: document.getElementById("input").value};
+    let input = { list: document.getElementById("input").value.trim() };
     array1.push(input);
-
     let listItems = document.querySelector(".lists");
     listItems.innerHTML = "";
-    array1.forEach((data) =>{
-        listItems.innerHTML += 
-        `<div class="list-item">
-            <input type="checkbox">
+    array1.forEach((data) => {
+        listItems.innerHTML +=
+            `<div class="list-item">
+            <input type="checkbox" id="check" onclick = "(function(){
+                let checkbox = document.getElementById("check");
+                if (checkbox.checked === true)
+                {
+                    document.querySelector("h3").style.textDecoration = "line-through";
+                } else {
+                    document.querySelector("h3").style.textDecoration = "none";
+                }
+            })();">
             <h3>${data.list}</h3>
-            <button id='x'>X</button>
+            <button id='sake'>X</button>
         </div>`;
     });
 });
+
+
+
+
+
