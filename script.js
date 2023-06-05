2d3976ac18b5ae989958918c9f4de0f730a47181
WaydinJochem <jochem7405@gmail.com>
2023-06-05 16:24:31 +0200

    Common

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
 
+const btnAdd = document.getElementById("add");
 let array1 = [];
-let btnAdd = document.getElementById("Add");
-let btnSort = document.getElementById("Sort");
 
-// list items and add the to array
-btnAdd.addEventListener("click", addData)
- 
-function addData() {
-    let input = {list: document.getElementById("input").value.trim()};
-    array1.push(input);
-    let listItems = document.querySelector(".lists");
-    listItems.innerHTML = "";
-    array1.forEach((data) =>{
-        listItems.innerHTML += 
-        `<div class="list-item">
-            <input type="checkbox" id="check">
-            <h3>${data.list}</h3>
-            <button id='sake'>X</button>
-        </div>`;
-    });
-    btnSort.addEventListener("click, clrData")
-    function clrData () {
-         
-    }
-}
+btnAdd.addEventListener("click", () {
+        let addInput = document.getElementById('box');
+        array1.push(addInput);
+        
+});
